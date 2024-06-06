80a1e98ff50fdb9dbdb749e639a9e2cca53d6e13
Tamal <tamalkundu2002@gmail.com>
2024-06-06 14:29:10 +0530

    commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/organization/:id",
+        destination: "/organization/:id/boards",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
